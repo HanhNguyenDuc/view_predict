f8eb250be4bc53bd0b620b07869fe4a674c5df08
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-08-22 08:35:58 +0700

    update

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.preprocessing import PolynomialFeatures, LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 # from sklearn import preprocessing as pre
 import pandas as pd  
 import matplotlib.pyplot as plt 
@@ -37,13 +39,21 @@ def train(filename):
     # X_train = scaler.fit_train
     # print(X_train)
     # X_test = np.expand_dims(X_test, axis = 1)
-    reg = MLPRegressor(
+    reg_mlp = MLPRegressor(
     hidden_layer_sizes=(250),  activation='relu', solver='adam', alpha=0.001, batch_size='auto',
     learning_rate='adaptive', learning_rate_init=0.01, power_t=0.5, max_iter=500, shuffle=True,
     random_state=9, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True,
-    early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08).fit(X_train, y_train)
-    print(reg.score(X_test, y_test))
-    y_predict = reg.predict(X_test)
+    early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+
+    # reg_svr = SVR(gamma = 'scale')
+    # reg_svr.fit(X_train, y_train)
+
+    reg_RFR = RandomForestRegressor(n_estimators = 50)
+    # reg_RFR.fit(X_train, y_train)
+    reg_GBR = GradientBoostingRegressor()
+    reg_mlp.fit(X_train, y_train)
+
+    y_predict = reg_mlp.predict(X_test)
     plt.plot(y_test)
     plt.plot(y_predict)
     plt.show()
